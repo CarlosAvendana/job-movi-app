12522e411aa4f8b21dd9b965591bb9a8fa6ef48b
Carlos Obando Avendaña <49579027+CarlosAvendana@users.noreply.github.com>
2020-04-30 21:13:41 -0600

    Creacion de archivos necesarios para el recycler

--- Form/app/src/main/java/com/example/form/Adaptador/FormAdapter.java
+++ Form/app/src/main/java/com/example/form/Adaptador/FormAdapter.java
@@ -1,4 +1,176 @@
 package com.example.form.Adaptador;
 
-public class FormAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.form.R;
+import com.example.form.logic.Form;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+/*
+public class FormAdapter extends RecyclerView.Adapter<FormAdapter.MyViewHolder> implements Filterable {
+    private List<Form> formList;
+    private List<Form> formListFiltered;
+    private FormAdapterListener listener;
+    private Form deletedItem;
+
+    public FormAdapter(List<Form> formList, FormAdapterListener listener) {
+        this.formList = formList;
+        this.listener = listener;
+        //init filter
+        this.formListFiltered = formList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+/*
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        // basically a render
+        final Form _form = formListFiltered.get(position);
+        holder.title1.setText(_form.getCodigo());
+        holder.title2.setText(_form.getNombre());
+        holder.description.setText(_form.getTitulo());
+    }
+
+    @Override
+    public int getItemCount() {
+        return formListFiltered.size();
+    }
+
+    public void removeItem(int position) {
+        deletedItem = formListFiltered.remove(position);
+        Iterator<Form> iter = formList.iterator();
+        while (iter.hasNext()) {
+            Form aux = iter.next();
+            if (deletedItem.equals(aux))
+                iter.remove();
+        }
+        // notify item removed
+        notifyItemRemoved(position);
+    }
+
+    public void restoreItem(int position) {
+
+        if (formListFiltered.size() == formList.size()) {
+            formListFiltered.add(position, deletedItem);
+        } else {
+            formListFiltered.add(position, deletedItem);
+            formList.add(deletedItem);
+        }
+        notifyDataSetChanged();
+        // notify item added by position
+        notifyItemInserted(position);
+    }
+
+    public Form getSwipedItem(int index) {
+        if (this.formList.size() == this.formListFiltered.size()) { //not filtered yet
+            return formList.get(index);
+        } else {
+            return formListFiltered.get(index);
+        }
+    }
+
+    public void onItemMove(int fromPosition, int toPosition) {
+        if (formList.size() == formListFiltered.size()) { // without filter
+            if (fromPosition < toPosition) {
+                for (int i = fromPosition; i < toPosition; i++) {
+                    Collections.swap(formList, i, i + 1);
+                }
+            } else {
+                for (int i = fromPosition; i > toPosition; i--) {
+                    Collections.swap(formList, i, i - 1);
+                }
+            }
+        } else {
+            if (fromPosition < toPosition) {
+                for (int i = fromPosition; i < toPosition; i++) {
+                    Collections.swap(formListFiltered, i, i + 1);
+                }
+            } else {
+                for (int i = fromPosition; i > toPosition; i--) {
+                    Collections.swap(formListFiltered, i, i - 1);
+                }
+            }
+        }
+        notifyItemMoved(fromPosition, toPosition);
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    formListFiltered = formList;
+                } else {
+                    List<Form> filteredList = new ArrayList<>();
+                    for (Form row : formList) {
+                        // filter use two parameters
+                        if (row.getCodigo().toLowerCase().contains(charString.toLowerCase()) || row.getNombre().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row);
+                        }
+                    }
+
+                    formListFiltered = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = formListFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                formListFiltered = (ArrayList<Form>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    public interface FormAdapterListener {
+        void onContactSelected(Form form);
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title1, title2, description;
+        //two layers
+        public RelativeLayout viewForeground, viewBackgroundDelete, viewBackgroundEdit;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title1 = view.findViewById(R.id.titleFirstLbl);
+            title2 = view.findViewById(R.id.titleSecLbl);
+            description = view.findViewById(R.id.descriptionLbl);
+            viewBackgroundDelete = view.findViewById(R.id.view_background_delete);
+            viewBackgroundEdit = view.findViewById(R.id.view_background_edit);
+            viewForeground = view.findViewById(R.id.view_foreground);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // send selected contact in callback
+                    listener.onContactSelected(formListFiltered.get(getAdapterPosition()));
+                }
+            });
+        }
+    }
+
+
 }
+*/
--- Form/app/src/main/java/com/example/form/logic/Form.java
+++ Form/app/src/main/java/com/example/form/logic/Form.java
@@ -2,5 +2,160 @@ package com.example.form.logic;
 
 public class Form {
 
+    private String _first_name;
+    private String _last_name;
+    private String _street_address;
+    private String _street_address2;
+    private String _city;
+    private String _state_province;
+    private String _postal_code;
+    private String _country;
+    private String _email_address;
+    private String _phone_number;
+    private String _applying_position;
+    private String _startDate;
 
+    public Form() {
+        this._first_name = "";
+        this._last_name = "";
+        this._street_address = "";
+        this._street_address2 = "";
+        this._city = "";
+        this._state_province = "";
+        this._postal_code = "";
+        this._country = "";
+        this._email_address = "";
+        this._phone_number = "";
+        this._applying_position = "";
+        this._startDate = "";
+    }
+
+    public Form(String _first_name, String _last_name, String _street_address, String _street_address2, String _city, String _state_province, String _postal_code, String _country, String _email_address, String _phone_number, String _applying_position, String _startDate) {
+        this._first_name = _first_name;
+        this._last_name = _last_name;
+        this._street_address = _street_address;
+        this._street_address2 = _street_address2;
+        this._city = _city;
+        this._state_province = _state_province;
+        this._postal_code = _postal_code;
+        this._country = _country;
+        this._email_address = _email_address;
+        this._phone_number = _phone_number;
+        this._applying_position = _applying_position;
+        this._startDate = _startDate;
+    }
+
+    public String get_first_name() {
+        return _first_name;
+    }
+
+    public void set_first_name(String _first_name) {
+        this._first_name = _first_name;
+    }
+
+    public String get_last_name() {
+        return _last_name;
+    }
+
+    public void set_last_name(String _last_name) {
+        this._last_name = _last_name;
+    }
+
+    public String get_street_address() {
+        return _street_address;
+    }
+
+    public void set_street_address(String _street_address) {
+        this._street_address = _street_address;
+    }
+
+    public String get_street_address2() {
+        return _street_address2;
+    }
+
+    public void set_street_address2(String _street_address2) {
+        this._street_address2 = _street_address2;
+    }
+
+    public String get_city() {
+        return _city;
+    }
+
+    public void set_city(String _city) {
+        this._city = _city;
+    }
+
+    public String get_state_province() {
+        return _state_province;
+    }
+
+    public void set_state_province(String _state_province) {
+        this._state_province = _state_province;
+    }
+
+    public String get_postal_code() {
+        return _postal_code;
+    }
+
+    public void set_postal_code(String _postal_code) {
+        this._postal_code = _postal_code;
+    }
+
+    public String get_country() {
+        return _country;
+    }
+
+    public void set_country(String _country) {
+        this._country = _country;
+    }
+
+    public String get_email_address() {
+        return _email_address;
+    }
+
+    public void set_email_address(String _email_address) {
+        this._email_address = _email_address;
+    }
+
+    public String get_phone_number() {
+        return _phone_number;
+    }
+
+    public void set_phone_number(String _phone_number) {
+        this._phone_number = _phone_number;
+    }
+
+    public String get_applying_position() {
+        return _applying_position;
+    }
+
+    public void set_applying_position(String _applying_position) {
+        this._applying_position = _applying_position;
+    }
+
+    public String get_startDate() {
+        return _startDate;
+    }
+
+    public void set_startDate(String _startDate) {
+        this._startDate = _startDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Form{" +
+                "_first_name='" + _first_name + '\'' +
+                ", _last_name='" + _last_name + '\'' +
+                ", _street_address='" + _street_address + '\'' +
+                ", _street_address2='" + _street_address2 + '\'' +
+                ", _city='" + _city + '\'' +
+                ", _state_province='" + _state_province + '\'' +
+                ", _postal_code='" + _postal_code + '\'' +
+                ", _country='" + _country + '\'' +
+                ", _email_address='" + _email_address + '\'' +
+                ", _phone_number='" + _phone_number + '\'' +
+                ", _applying_position='" + _applying_position + '\'' +
+                ", _startDate='" + _startDate + '\'' +
+                '}';
+    }
 }
